2eaca1a03f409bfc335d5baea9535079a00d3f36
Jean-Baptiste Feldis <jb.feldis@studiomelipone.eu>
2011-05-12 21:37:31 +0200

    Added user_id to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110512193347) do
+ActiveRecord::Schema.define(:version => 20110512193711) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20110512193347) do
     t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
