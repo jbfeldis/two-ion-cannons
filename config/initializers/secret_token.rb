f59aa495a422c4919fa2a5a233f39eac18773418
Jean-Baptiste Feldis <jb.feldis@studiomelipone.eu>
2011-05-12 21:28:32 +0200

    First commit, removed public/index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwoIonCanons::Application.config.secret_token = '80a1546218f7133f66ee98c1b63fd7230b546145376863c7d4607e94957c68d6e7a4695cc25eab2218d2e5df9b7807d56c2e9e1a2c30604182399949363323d3'
